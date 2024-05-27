f42e45600a83da9c088859a65c96228905ead42d
akom <akom707@gmail.com>
2024-05-27 10:27:26 +0300

    improv

--- main.js
+++ main.js
@@ -49,11 +49,73 @@ education.onmouseout = function () {
 const about = document.querySelector("#about")
 
 about.onmouseover = function () {
-    about.innerText = `Highly motivated and results-oriented Web Developer with a
-    strong foundation in web development principles gained through
-    an online web development bootcamp and Tuwaiq academy
-    courses`
+    about.innerText = `Highly motivated and results-oriented Web Developer with a strong foundation in web development principles gained through an online web development bootcamp and Tuwaiq academy courses`
 }
 about.onmouseout = function () {
     about.innerText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequuntur architecto alias assumenda fugit, soluta error, dolorem tempora ex consectetur recusandae cum facilis natus illum. Sint delectus cumque optio fugiat? Perferendis ipsum dolore animi at maxime ipsam quidem magni alias id necessitatibus quibusdam quis suscipit ipsa et nam reiciendis nobis doloribus a est, nisi praesentium impedit excepturi odio. Repellendus, quia! Earum excepturi laboriosam sit minima adipisci incidunt delectus ratione, fugiat rem eveniet ipsum non voluptates ut numquam in sapiente voluptatum dolorum distinctio quis accusamus architecto. Animi nam inventore architecto dolorum!`
+}
+
+//-----------------------------------------
+const job = document.querySelector("#job")
+job.onmouseover = function () {
+    job.innerText = "WEB DEVELOPER"
+}
+job.onmouseout = function () {
+    job.innerText = "job"
+}
+//-----------------------------------------
+const lang1 = document.querySelector("#lang1")
+lang1.onmouseover = function () {
+    lang1.innerText = "Arabic : excellent"
+}
+lang1.onmouseout = function () {
+    lang1.innerText = "lorem : lorem"
+}
+//-----------------------------------------
+const lang2 = document.querySelector("#lang2")
+lang2.onmouseover = function () {
+    lang2.innerText = "English : good"
+}
+lang2.onmouseout = function () {
+    lang2.innerText = "lorem : lorem"
+}
+//-----------------------------------------
+const itme1 = document.querySelector(".itme1")
+itme1.onmouseover = function () {
+    itme1.innerText = "Javascript"
+}
+itme1.onmouseout = function () {
+    itme1.innerText = "lorem"
+}
+//-----------------------------------------
+const itme2 = document.querySelector(".itme2")
+itme2.onmouseover = function () {
+    itme2.innerText = "node.js"
+}
+itme2.onmouseout = function () {
+    itme2.innerText = "lorem"
+}
+//-----------------------------------------
+const itme3 = document.querySelector(".itme3")
+itme3.onmouseover = function () {
+    itme3.innerText = "front end dev"
+}
+itme3.onmouseout = function () {
+    itme3.innerText = "lorem"
+}
+//-----------------------------------------
+const itme4 = document.querySelector(".itme4")
+itme4.onmouseover = function () {
+    itme4.innerText = "back end dev"
+}
+itme4.onmouseout = function () {
+    itme4.innerText = "lorem"
+}
+//-----------------------------------------
+const itme5 = document.querySelector(".itme5")
+itme5.onmouseover = function () {
+    itme5.innerText = "cloud computing"
+}
+itme5.onmouseout = function () {
+    itme5.innerText = "lorem"
 }
