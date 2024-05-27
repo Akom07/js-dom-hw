bde10a05e194450377fd965ae76521fd1be7a6b8
akom <akom707@gmail.com>
2024-05-27 09:25:06 +0300

    done

---
+++ main.js
@@ -0,0 +1,59 @@
+const name1 = document.querySelector("#name1")
+name1.onmouseover = function () {
+    name1.innerText = "amer alghamdi"
+}
+name1.onmouseout = function () {
+    name1.innerText = "name"
+}
+//------------------------------
+const phone = document.querySelector("#phone")
+
+phone.onmouseover = function () {
+    phone.innerText = "phone :0540545524"
+}
+phone.onmouseout = function () {
+    phone.innerText = "phone :123456789"
+}
+//----------------------------------
+const email = document.querySelector("#email")
+
+email.onmouseover = function () {
+    email.innerText = "email: akom707@gmail.com"
+}
+email.onmouseout = function () {
+    email.innerText = "email: 123@gmail.com"
+}
+//----------------------------------                        
+const gitHub = document.querySelector("#gitHub")
+
+gitHub.onmouseover = function () {
+    gitHub.innerText = "gitHub"
+    gitHub.href = "https://github.com/Akom07?tab=repositories"
+}
+gitHub.onmouseout = function () {
+    gitHub.innerText = "Git Hub"
+}
+//----------------------------------
+const education = document.querySelector("#education")
+
+education.onmouseover = function () {
+    education.innerText = `Bachelor’s Degree in Computer Science
+    Very Good with Honors Second Class
+    `
+
+}
+education.onmouseout = function () {
+    education.innerText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, excepturi? Nulla delectus ex quidem non,recusandae, amet dolore impedit iste fuga autem nihil at dolor magnam inventore incidunt nostrum in."
+}
+//---------------------------------------
+const about = document.querySelector("#about")
+
+about.onmouseover = function () {
+    about.innerText = `Highly motivated and results-oriented Web Developer with a
+    strong foundation in web development principles gained through
+    an online web development bootcamp and Tuwaiq academy
+    courses`
+}
+about.onmouseout = function () {
+    about.innerText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequuntur architecto alias assumenda fugit, soluta error, dolorem tempora ex consectetur recusandae cum facilis natus illum. Sint delectus cumque optio fugiat? Perferendis ipsum dolore animi at maxime ipsam quidem magni alias id necessitatibus quibusdam quis suscipit ipsa et nam reiciendis nobis doloribus a est, nisi praesentium impedit excepturi odio. Repellendus, quia! Earum excepturi laboriosam sit minima adipisci incidunt delectus ratione, fugiat rem eveniet ipsum non voluptates ut numquam in sapiente voluptatum dolorum distinctio quis accusamus architecto. Animi nam inventore architecto dolorum!`
+}
